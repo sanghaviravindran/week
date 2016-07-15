3a59d14d2e1738a03dcb97efcd265bfd94674efc
sanghaviravindran <sangu.abirami@gmail.com>
2016-07-15 23:04:23 +0530

    Add files via upload

---
+++ week.java
@@ -0,0 +1,15 @@
+public class Season {
+    public static void main(String[] args) {
+ 
+        String s;
+        Scanner sc=new Scanner(System.in);
+        s=sc.nextLine();
+              if(s.equals("sunday"))
+            {
+                System.out.println("false");
+    }
+              else
+                  System.out.println("true");
+  
+}  
+} 
