072b22e0da21db10db949986f7161875acea641e
sanghaviravindran <sangu.abirami@gmail.com>
2016-07-15 23:24:45 +0530

    Add files via upload

--- week.java
+++ week.java
@@ -1,15 +1,19 @@
-public class Season {
-    public static void main(String[] args) {
- 
-        String s;
-        Scanner sc=new Scanner(System.in);
-        s=sc.nextLine();
-              if(s.equals("sunday"))
-            {
-                System.out.println("false");
-    }
-              else
-                  System.out.println("true");
-  
-}  
-} 
+public class Days{
+
+public static void main(String ar[])throws Exception
+{
+String s1;
+Scanner sc=new Scanner(System.in);
+s1=sc.next();
+
+ check(s1);
+}
+public static void check(String da)
+{
+if(da.equalsIgnoreCase("Sunday"))
+{
+System.out.println("false");}
+else
+System.out.println("true");
+}
+}
